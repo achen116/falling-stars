973d52290f763d301335be33649fbbb88fe270e5
achen116 <chen.arielle@gmail.com>
2015-07-11 12:17:19 -0700

    update folders

--- public/assets/js/game.js
+++
@@ -1,5 +0,0 @@
-// THE GAME STATE
-// It is handled by game.js. 
-// This is the actual play area of the game. 
-// [Example Game: You control the snake, eat apples and have lots of fun. 
-// When you die, you transition to the Game_Over state.]
